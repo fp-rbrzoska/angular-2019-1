b3ff86cdccaf380fdf6b1426687fa6f7c12286b6
Rafal Brzoska <rbrzoska@future-processing.com>
2019-04-23 11:59:30 +0200

    first components

--- fp-app/src/app/app.component.ts
+++ fp-app/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<app-test></app-test>
+  <app-home></app-home>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- fp-app/src/app/app.module.ts
+++ fp-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
