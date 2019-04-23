49e54f04c3dd51b7013eb793770c6d7ee9667603
Rafal Brzoska <rbrzoska@future-processing.com>
2019-04-23 12:46:51 +0200

    removed test component

--- fp-app/src/app/app.component.ts
+++ fp-app/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-test></app-test>
-  <app-home></app-home>`,
+  template: `<app-home></app-home>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- fp-app/src/app/app.module.ts
+++ fp-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
@@ -11,7 +10,6 @@ import { ContactComponent } from './contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     HomeComponent,
     AboutComponent,
     ContactComponent
